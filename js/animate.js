17e085301f32dc755caffdc3051cd08ce7a6a3cd
Arcdev <colibaoallanreyes@gmail.com>
2020-01-25 15:33:29 +0800

    commit

---
+++ js/animate.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#loader').fadeOut(6000);
+    $(".h-1").animate({
+        left: '0'
+    }, 7000);
+    $(".h-2").animate({
+        left: '0'
+    }, 8000);
+    $(".h-3").animate({
+        left: '0'
+    }, 9000);
+});
